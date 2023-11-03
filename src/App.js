fc8cecbcca035d681859e3e832af007b8bd7879f
GevorAbrahamyan1991 <abgevor@gmail.com>
2023-11-03 19:36:40 +0400

    Add About.js File

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Route, Routes, route } from "react-router";
 import Home from "./pages/Home";
+import About from "./pages/About";
 function App() {
   return (
     <>
       <Routes>
         <Route path="" element={<Home />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </>
   );
