3d5a730403ef0a46dfd5090f5dd67fca30b1e7f8
GevorAbrahamyan1991 <abgevor@gmail.com>
2023-11-03 19:37:44 +0400

    Add News.js File

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Route, Routes, route } from "react-router";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import News from "./pages/News";
 function App() {
   return (
     <>
       <Routes>
         <Route path="" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/news" element={<News />} />
       </Routes>
     </>
   );
